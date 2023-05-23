ae0fad5a567025032311e0686ab361dc8450f9bb
Natalia Mota <nataliamotand@gmail.com>
2023-05-23 16:40:52 -0300

    feat: calculo media avaliacoes

--- src/Controllers/ComentarioController.js
+++ src/Controllers/ComentarioController.js
@@ -1,15 +1,21 @@
 const ComentarioModel = require("../Models/ComentarioModel");
-const UsuarioModel = require('../Models/UsuarioModel');
+const UsuarioModel = require("../Models/UsuarioModel");
 
 class ComentarioController {
-
   async readByLocal(req, res) {
     const { id_local } = req.params;
 
-    const comentarios = await ComentarioModel.find({ id_local: id_local })
-      .populate("id_usuario", "nome");
+    const comentarios = await ComentarioModel.find({
+      id_local: id_local,
+    }).populate("id_usuario", "nome");
 
-    return res.status(200).json(comentarios);
+    let total_avaliacao = 0;
+    for (const comentario of comentarios) {
+      total_avaliacao += parseInt(comentario.avaliacao, 10);
+    }
+    const media_avaliacao = total_avaliacao / comentarios.length;
+
+    return res.status(200).json({ comentarios, media_avaliacao });
   }
 }
 
