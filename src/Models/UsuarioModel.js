efa68661828e8e86aac986f5e2766949b0350a1d
Natalia Mota <nataliamotand@gmail.com>
2023-06-07 09:47:15 -0300

    feat: preparacao tarefa

--- src/Models/UsuarioModel.js
+++ src/Models/UsuarioModel.js
@@ -1,50 +1,11 @@
 const mongoose = require("mongoose");
- 
+
 const Schema = mongoose.Schema;
 
-const UsuarioSchema = new Schema ({
-    nome: {
-        type: String,
-        required: true,
-      },
-      telefone: {
-        type: String,
-        required: true,
-        unique: true, 
-      },
-      data_nascimento: {
-        type: Date,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique : true,
-      },
-      senha: {
-        type: String,
-        required: true,
-      },
-      avatar_url: {
-        type: String,
-        required: false,
-      },
-      crm: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      uni_federativa: {
-        type: String,
-        required: true,
-      },
-      admin: {
-        type: Boolean,
-        required: true,
-        default: false
-      },
-})
+const UsuarioSchema = new Schema({
+  
+});
 
-const UsuarioModel = mongoose.model('usuarios', UsuarioSchema);
+const UsuarioModel = mongoose.model("usuarios", UsuarioSchema);
 
 module.exports = UsuarioModel;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,4 @@ const UsuarioValidator = require("./Validators/UsuarioValidator");
 
 const rotas = Router();
 
-rotas.post('/usuarios', UsuarioValidator.create, UsuarioController.create);
-rotas.get('/usuarios', UsuarioController.read);
 module.exports = rotas;
