6b000518a194f8ad7274a66b285d4420d422a9b1
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-06-16 14:17:23 -0300

    feat: criação do cadastro do usuário e do get de usuários para teste

--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -2,7 +2,22 @@ const UsuarioModel = require("../Models/UsuarioModel");
 
 class UsuarioController {
   async create(req, res) {
-
+    try {
+      const usuario = await UsuarioModel.create(req.body);
+      const {senha, ...usuarioNovo} = usuario.toObject();
+      res.status(200).json(usuarioNovo); 
+    } catch (error) {
+      res.status(500).json({message: "Erro na criação do usuário", error: error.message});
+    }
+  }
+  async read(req, res) {
+    try {
+      const usuario = await UsuarioModel.find();
+      res.status(200).json(usuario);
+      
+    } catch (error) {
+      res.status(500).json({message: "Erro na obtenção dos usuários", error: error.message});
+    }
   }
 }
 
--- src/Models/UsuarioModel.js
+++ src/Models/UsuarioModel.js
@@ -1,9 +1,47 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
 const UsuarioSchema = new Schema({
-  
+  nome: {
+    type: String,
+    unique: true,
+  },
+  telefone: {
+    type: Number,
+    unique: true,
+  }, 
+  crm: {
+    type: Number,
+    unique: true,
+  },
+  avatar_url: {
+    type: String,
+  },
+  data_nascimento: {
+    type: Date  
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  uni_federativa: {
+    type: String
+  },
+  senha: {
+    type: String,
+    select: false
+  }
+});
+
+UsuarioSchema.pre("save", async function(next){
+  if (this.isModified("senha")) {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(this.senha, salt);
+    this.senha = hash;
+  }
+  next();
 });
 
 const UsuarioModel = mongoose.model("usuarios", UsuarioSchema);
