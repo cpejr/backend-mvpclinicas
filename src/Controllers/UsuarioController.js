37961c19ef64c7ed41cc054b407c13a31fda93cd
Unknown <unknown@example.com>
2023-06-19 17:35:43 -0300

    feat: funções para update dos dados
    função update no UsuarioController e nas rodas. Ainda com alguns problemas que precisam ser resolvidos.

--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -14,6 +14,22 @@ class UsuarioController {
 
     return res.status(200).json(usuario);
   }
+
+  async update(req, res){
+    try{
+        const { id } = req.params;
+        const usuarioEncontrado = await UsuarioModel.findById(id);
+
+        if (!usuarioEncontrado) 
+            return res.status(404).json({ message: "Usuário não encontrado." });
+        
+        const usuario = await usuarioEncontrado.set(req.body).save();
+
+        return res.status(200).json(usuario);
+    } catch(error){
+        res.status(500).json({message: "Deu ruim aqui!", error: error.message});
+    }
+}
 }
 
 module.exports = new UsuarioController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const UsuarioController = require("./Controllers/UsuarioController");
 const rotas = Router();
 
 rotas.get('/usuarios', UsuarioController.read);
+rotas.put('/usuarios/:id', UsuarioController.update);
 rotas.get('/usuarios/:id', UsuarioController.readById);
 
 module.exports = rotas;
