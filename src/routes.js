d6ccd67458ab0e1f642145242e8ed2dc4841c0ad
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-07-13 22:55:41 -0300

    feat: local validator

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,15 @@ const { Router } = require("express");
 const LocalController = require("./Controllers/LocalController");
 const ComentarioController = require("./Controllers/ComentarioController");
 const UsuarioController = require("./Controllers/UsuarioController");
+const LocalValidator = require("./Validators/LocalValidator");
 
 const rotas = Router();
 
 rotas.get('/locais', LocalController.read);
 rotas.get('/locais/:id_local', LocalController.readById);
-rotas.post('/locais', LocalController.create);
+rotas.post('/locais', 
+LocalValidator.create,
+LocalController.create);
 
 rotas.get('/usuarios', UsuarioController.read);
 rotas.get('/usuarios/:id', UsuarioController.readById);
