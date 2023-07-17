2de344d36f529b11e6b8b93f1fbc07209caedc1e
SamuelFVG <samuelfelipeif@gmail.com>
2023-07-17 16:24:35 -0300

    fix: correções das revisões

--- src/Controllers/ComentarioController.js
+++ src/Controllers/ComentarioController.js
@@ -1,6 +1,6 @@
 const ComentarioModel = require("../Models/ComentarioModel");
 const LocalModel = require("../Models/LocalModel");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 class ComentarioController {
   async readByLocal(req, res) {
@@ -18,26 +18,29 @@ class ComentarioController {
 
     return res.status(200).json({ comentarios, media_avaliacao });
   }
-  async create(req,res) {
+  async create(req, res) {
     const { id_local } = req.params;
     const data = req.body;
 
-    console.log(id_local)
-    const local = await LocalModel.findById(new mongoose.Types.ObjectId(id_local));
-    console.log(local)
-    if (!local) return res.status(400).json({message: "O id do local não pode ser encontrado!"})
+    const local = await LocalModel.findById(
+      new mongoose.Types.ObjectId(id_local)
+    );
+    if (!local)
+      return res
+        .status(400)
+        .json({ message: "O id do local não pode ser encontrado!" });
 
     const comentario = await ComentarioModel.create({
       id_usuario: data.id_usuario,
       id_local: id_local,
       avaliacao: data.avaliacao,
-      comentario: data.comentario
-    })
+      comentario: data.comentario,
+    });
 
-    if (!comentario) return res.status(500).json({message: "Problema no servidor!"})
+    if (!comentario)
+      return res.status(500).json({ message: "Problema no servidor!" });
 
-    
-    return res.status(201).json({comentario});
+    return res.status(201).json({ comentario });
   }
 }
 
