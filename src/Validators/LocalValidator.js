ed7d6fea2173d25943ca8773457fa2e0f7a294c5
Unknown <unknown@example.com>
2023-07-25 20:04:07 -0300

    feat: Verificação do usuário ser administrador

--- src/Validators/LocalValidator.js
+++ src/Validators/LocalValidator.js
@@ -1,11 +1,32 @@
 const { z } = require("zod");
 const { validateRequest } = require("zod-express-middleware");
 const { default: mongoose } = require("mongoose");
+const UsuarioModel = require("../Models/UsuarioModel");
+
+const ConfereAdmin = async (req, res, next) => {
+    const { id_usuario } = req.params;
+
+    try{
+        const usuario = await UsuarioModel.findOne({ id_usuario }).exec();
+        if(!usuario) {
+            return res.status(404).json({ message: "Usuario não encontrado." });
+        }
+
+        if(!usuario.admin) {
+            return res.status(403).json({ message: "Apenas administradores podem realizar esta operação." });
+        }
+
+        next();
+    } catch(err){
+        console.error("Erro ao verificar a permissão do usuario.");
+        return res.status(500).json({ message: "Erro ao verificar a permissão do usuario." });
+    }
+}
 
 const destroy = validateRequest({
     params: z.object({
-        id: z.costum(mongoose.isValidObjectId, "o id não é válido para esta operação."),
+        id: z.custom(mongoose.isValidObjectId, "o id não é válido para esta operação."),
     }),
 });
 
-module.exports = { destroy };
+module.exports = { destroy, ConfereAdmin };
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ const LocalController = require("./Controllers/LocalController");
 const ComentarioController = require("./Controllers/ComentarioController");
 const UsuarioController = require("./Controllers/UsuarioController");
 const ComentarioValidator = require("./Validators/ComentarioValidator");
+const LocalValidator = require("./Validators/LocalValidator");
+const UsuarioModel = require("./Models/UsuarioModel");
 
 const rotas = Router();
 
 rotas.get("/locais", LocalController.read);
 rotas.get("/locais/:id_local", LocalController.readById);
-rotas.delete("/locais/:id_local", LocalController.destroy);
+rotas.delete("/locais/:id_local", LocalController.destroy, LocalValidator.destroy, LocalValidator.ConfereAdmin);
 
 rotas.get("/usuarios", UsuarioController.read);
 rotas.get("/usuarios/:id", UsuarioController.readById);
@@ -17,10 +19,23 @@ rotas.get("/locais", LocalController.read);
 rotas.get("/locais/:id_local", LocalController.readById);
 
 rotas.get("/comentarios/:id_local", ComentarioController.readByLocal);
-rotas.post(
-  "/comentarios/:id_local",
-  ComentarioValidator.create,
-  ComentarioController.create
-);
+rotas.post("/comentarios/:id_local",ComentarioValidator.create, ComentarioController.create);
+
+rotas.get("/:id_usuario/admin", async (req, res) => {
+    const { id_usuario } = req.params;
+  
+    try {
+      const usuario = await UsuarioModel.findById(id_usuario);
+      if (!usuario) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+  
+      const ehAdmin = usuario.admin;
+      res.json({ ehAdmin });
+    } catch (err) {
+      console.error("Erro ao verificar permissão do usuário:", err);
+      res.status(500).json({ message: "Erro ao verificar permissão do usuário." });
+    }
+});
 
 module.exports = rotas;
