0414cee26ae7d94081dd443536421a92f9457ee0
SamuelFVG <samuelfelipeif@gmail.com>
2023-07-27 12:26:31 -0300

    fix: checklists

--- src/Validators/LocalValidator.js
+++ src/Validators/LocalValidator.js
@@ -4,29 +4,37 @@ const { default: mongoose } = require("mongoose");
 const UsuarioModel = require("../Models/UsuarioModel");
 
 const ConfereAdmin = async (req, res, next) => {
-    const { id_usuario } = req.params;
+  const { id_usuario } = req.params;
 
-    try{
-        const usuario = await UsuarioModel.findOne({ id_usuario }).exec();
-        if(!usuario) {
-            return res.status(404).json({ message: "Usuario não encontrado." });
-        }
-
-        if(!usuario.admin) {
-            return res.status(403).json({ message: "Apenas administradores podem realizar esta operação." });
-        }
-
-        next();
-    } catch(err){
-        console.error("Erro ao verificar a permissão do usuario.");
-        return res.status(500).json({ message: "Erro ao verificar a permissão do usuario." });
+  try {
+    const usuario = await UsuarioModel.findOne({ id_usuario }).exec();
+    if (!usuario) {
+      return res.status(404).json({ message: "Usuario não encontrado." });
     }
-}
+
+    if (!usuario.admin) {
+      return res
+        .status(403)
+        .json({
+          message: "Apenas administradores podem realizar esta operação.",
+        });
+    }
+
+    next();
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Erro ao verificar a permissão do usuario." });
+  }
+};
 
 const destroy = validateRequest({
-    params: z.object({
-        id: z.custom(mongoose.isValidObjectId, "o id não é válido para esta operação."),
-    }),
+  params: z.object({
+    id: z.custom(
+      mongoose.isValidObjectId,
+      "o id não é válido para esta operação."
+    ),
+  }),
 });
 
 module.exports = { destroy, ConfereAdmin };
