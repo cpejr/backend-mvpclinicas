10df1b91941000a37b8c6a27490d59aff83453c7
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-07-31 11:46:48 -0300

    feat: Rotas privadas

--- src/routes.js
+++ src/routes.js
@@ -17,37 +17,43 @@ const rotas = Router();
 
 rotas.post("/usuarios", UsuarioValidator.create, UsuarioController.create);
 rotas.get("/usuarios", verificarJwt, UsuarioController.read);
-rotas.get("/usuarios/:id", UsuarioController.readById);
+rotas.get("/usuarios/:id", verificarJwt, UsuarioController.readById);
 
-rotas.post("/locais", LocalValidator.create, LocalController.create);
-rotas.get("/locais", LocalController.read);
-rotas.get("/locais/:id_local", LocalController.readById);
+rotas.post("/locais", verificarJwt, LocalValidator.create, LocalController.create);
+rotas.get("/locais", verificarJwt, LocalController.read);
+rotas.get("/locais/:id_local", verificarJwt, LocalController.readById);
 
 rotas.post("/login", AuthValidator.login, AuthController.login);
 
 rotas.put(
   "/usuariosimagem/:id",
+  verificarUsuario,
+  verificarJwt,
   UsuarioValidator.updateImagem,
   UsuarioController.updateImagem
 );
 rotas.get(
   "/usuariosimagem/:id",
+  verificarJwt,
   //UsuarioValidator.pegarImagem,
   UsuarioController.pegarImagem
 );
 
-rotas.get("/comentarios/:id_local", ComentarioController.readByLocal);
+rotas.get("/comentarios/:id_local", verificarJwt, ComentarioController.readByLocal);
 rotas.get(
   "/comentarios/usuario/:id_usuario",
+  verificarJwt,
   ComentarioController.readByUsuario
 );
 rotas.post(
   "/comentarios/:id_local",
+  verificarJwt,
   ComentarioValidator.create,
   ComentarioController.create
 );
 rotas.delete(
   "/comentarios/:id_comentario",
+  verificarJwt,
   ComentarioValidator.destroy,
   ComentarioController.destroy
 );
