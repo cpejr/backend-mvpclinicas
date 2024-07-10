8e9a1ae7814da20cb9296e6d53aaeaa5ec616420
SamuelFVG <samuelfelipeif@gmail.com>
2024-07-10 14:51:00 -0300

    feat: add code to replace images

--- src/Controllers/LocalController.js
+++ src/Controllers/LocalController.js
@@ -1,6 +1,49 @@
 const LocalModel = require("../Models/LocalModel");
 const ComentarioModel = require("../Models/ComentarioModel");
 
+async function getPlaceImage(placeName, placeAddress) {
+  const GOOGLE_API_KEY = "AIzaSyBUwXbN66GC9i-ZGfQmEY8n_QXGytWBe6I";
+  const fetch = (await import("node-fetch")).default;
+
+  const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(
+    placeName
+  )}&key=${GOOGLE_API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.results && data.results.length > 0) {
+      const place = data.results[0];
+      const photoReference = place.photos
+        ? place.photos[0].photo_reference
+        : null;
+
+      const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${GOOGLE_API_KEY}`;
+      return photoUrl;
+    } else {
+      const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(
+        placeAddress
+      )}&key=${GOOGLE_API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.results && data.results.length > 0) {
+        const place = data.results[0];
+        const photoReference = place.photos
+          ? place.photos[0].photo_reference
+          : null;
+
+        const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${GOOGLE_API_KEY}`;
+        return photoUrl;
+      }
+    }
+    return null;
+  } catch (error) {
+    console.error("Error fetching place image:", error);
+    return null;
+  }
+}
+
 class LocalController {
   async read(req, res) {
     try {
@@ -21,9 +64,12 @@ class LocalController {
             mediaAvaliacoes = somaAvaliacoes / total_avaliacao;
           }
 
+          const image = await getPlaceImage(local.nome, local.endereco);
+
           const localComMedia = {
             ...local.toObject(),
             mediaAvaliacoes,
+            image,
           };
 
           return localComMedia;
@@ -40,10 +86,10 @@ class LocalController {
     const { id_local } = req.params;
 
     const local = await LocalModel.findById(id_local);
+    const imagem = await getPlaceImage(local.nome);
 
-    return res.status(200).json(local);
+    return res.status(200).json({ ...local, imagem });
   }
-
 
   async create(req, res) {
     try {
